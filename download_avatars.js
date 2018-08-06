5c168382d92ad1966dd66c316e6e6a633de70678
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-06 23:37:37 +0000

    Logging avatar URLs

--- download_avatars.js
+++ download_avatars.js
@@ -1,21 +1,25 @@
 var request = require('request');
-var secrets = require('secrets.js');
+var secrets = require('./secrets.js');
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorizaion': secrets.GITHUB_TOKEN
+      'Authorization': secrets
     }
   };
 
-  request(options.url, function(err, res, body) {
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors: ", err);
-  console.log("Result: ", result);
+  var parsedJSON = JSON.parse(result);
+  parsedJSON.forEach(function(obj) {
+    console.log("URLs: ", obj.avatar_url);
+  });
 });
