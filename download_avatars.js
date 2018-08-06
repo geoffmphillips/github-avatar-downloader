9aba8072177df8b4e468384c9ab178c893bc4bc4
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-06 22:55:53 +0000

    Added authorization to download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,18 @@
 var request = require('request');
+var secrets = require('secrets.js');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorizaion': secrets.GITHUB_TOKEN
+    }
+  };
 
+  request(options.url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
