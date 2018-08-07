98b819f4c7b60f4c9a7cf330833ae0067fd4920d
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-07 03:49:58 +0000

    Added ability to create directory if it does not exist. Throws user error if incorrect number of args passed by user or if repo/owner does not exist. Throws error if .env file has incorrect github token.

--- download_avatars.js
+++ download_avatars.js
@@ -1,21 +1,32 @@
+var fs = require('fs');
 var request = require('request');
-var secrets = require('./secrets.js');
-var dotenv = require('dotenv').config();
 var downloadImages = require('./downloadImages.js');
+require('dotenv').config();
 
 var args = process.argv.slice(2);
-var token = process.env.GITHUB_TOKEN;
 var repoOwner = args[0];
 var repoName = args[1];
 
+// Creates ./avatars directory if it doesn't exist
+fs.access("avatars", function(err) {
+  if (err && err.code === "ENOENT") {
+    fs.mkdir("avatars", function(error) {
+      if (error) {
+        console.log("Error: ", error);
+      }
+    });
+  }
+});
 
 function getRepoContributors(owner, name, cb) {
-  if (owner === undefined || name === undefined) {
-    console.log("Please enter a valid repo owner and name");
+  // Throws user error if user does not input both a repo owner and repo name
+  if (args.length < 2) {
+    console.log("Please enter both a repo owner and repo name");
     return;
   }
 
-  if (args.length > 2) {
+  // Throws error if user inputs too many arguments
+  if (args.length >= 3) {
     console.log("Too many words! Please only enter a repo owner and repo name.");
     return;
   }
@@ -24,11 +35,27 @@ function getRepoContributors(owner, name, cb) {
     url: "https://api.github.com/repos/" + owner + "/" + name + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': token
+      'Authorization': process.env.GITHUB_TOKEN
     }
   };
 
+  // Creates and parses JSON, passes it to callback function
   request(options, function(err, res, body) {
+
+    // Gives user errors if status is 404 (invalid repo owner or name) or 401 (bad github token)
+    var status = res.caseless.dict.status;
+    switch (status) {
+      case "404 Not Found":
+        console.log("Error: ", status, "\nPlease enter a valid repo owner and name.");
+        return;
+      case "401 Unauthorized":
+        console.log("Error: ", status, "\nPlease use valid github token.");
+        return;
+      default:
+        break;
+    }
+
+    // Passes parsedJSON to callback function
     var parsedJSON = JSON.parse(body);
     cb(err, parsedJSON);
   });
